56108d2761e65c152b54d588930997bc8e79a11f
Gal Schlezinger <gal@spitfire.co.il>
2020-10-25 10:21:17 +0200

    fnm v1.22.1 (new formula)

---
+++ Formula/fnm.rb
@@ -0,0 +1,53 @@
+class Fnm < Formula
+  desc "Fast and simple Node.js version manager"
+  homepage "https://github.com/Schniz/fnm"
+  url "https://github.com/Schniz/fnm/archive/v1.22.1.tar.gz"
+  sha256 "5e87db45c4c103eed0e4b64b4fadd7d9b7cdaccc00c3b5761556f17b32835c64"
+  license "GPL-3.0-only"
+  head "https://github.com/Schniz/fnm.git"
+
+  livecheck do
+    url "https://github.com/Schniz/fnm/releases/latest"
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
+  depends_on "rust" => :build
+
+  uses_from_macos "zlib"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    (bash_completion/"fnm").write `#{bin}/fnm completions --shell bash`
+    (fish_completion/"fnm.fish").write `#{bin}/fnm completions --shell fish`
+    (zsh_completion/"_fnm").write `#{bin}/fnm completions --shell zsh`
+  end
+
+  def caveats
+    <<~CAVEATS
+      Thanks for installing fnm!
+      The last step for making fnm work is to run it on the shell startup, using the `fnm env` command.
+
+      In order to complete the installation, please add the following to your shell profile:
+
+        # fnm
+        #{source_for_shell}
+
+      Homebrew tells us that #{preferred} is your preferred shell,
+      and your shell profile is at #{shell_profile.inspect}, if that helps 😇
+    CAVEATS
+  end
+
+  test do
+    system("#{bin}/fnm", "install", "12.0.0")
+    assert_match "v12.0.0", shell_output("#{bin}/fnm exec --using=12.0.0 -- node --version")
+  end
+
+  def source_for_shell
+    if preferred == :fish
+      "fnm env --multi | source"
+    else
+      'eval "$(fnm env --multi)"'
+    end
+  end
+end
